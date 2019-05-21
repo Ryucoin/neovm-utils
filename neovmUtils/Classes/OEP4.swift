ec287b21e5aa75fb11fd0e94adc1bd87f3b17cea
Wyatt Mufson <wyatt@ryucoin.com>
2019-05-21 23:02:09 +0300

    OEP4 transfer and transferMulti

--- neovmUtils/Classes/OEP4.swift
+++ neovmUtils/Classes/OEP4.swift
@@ -43,4 +43,48 @@ public class OEP4Interface: NSObject {
         let hex = ontologyInvokeRead(endpoint: endpoint, contractHash: contractHash, method: "balanceOf", args: [address])
         return hex.hexToDecimal()
     }
+
+    public func transfer(from: String, to: String, amount: Double, decimals: Int, gasPrice: Int = 500, gasLimit: Int = 20000, wallet: Wallet) -> String {
+        return transfer(from: from, to: to, amount: amount, decimals: decimals, gasPrice: gasPrice, gasLimit: gasLimit, wif: wallet.wif)
+    }
+
+    public func transfer(from: String, to: String, amount: Double, decimals: Int, gasPrice: Int = 500, gasLimit: Int = 20000, wif: String) -> String {
+        let fromAcct = OntologyParameter(type: .Address, value: from)
+        let toAcct = OntologyParameter(type: .String, value: to)
+        var type = OntologyParameterType.Integer
+        if decimals == 8 {
+            type = .Fixed8
+        } else if decimals == 9 {
+            type = .Fixed9
+        }
+        let spend = OntologyParameter(type: type, value: amount)
+        return ontologyInvoke(endpoint: endpoint, contractHash: contractHash, method: "transfer", args: [fromAcct, toAcct, spend], gasPrice: gasPrice, gasLimit: gasLimit, wif: wif)
+    }
+
+    public func transferMulti(args: [[String]], decimals: Int, gasPrice: Int = 500, gasLimit: Int = 20000, wallet: Wallet) -> String {
+        return transferMulti(args: args, decimals: decimals, gasPrice: gasPrice, gasLimit: gasLimit, wif: wallet.wif)
+    }
+
+    public func transferMulti(args: [[String]], decimals: Int, gasPrice: Int = 500, gasLimit: Int = 20000, wif: String) -> String {
+        var params: [OntologyParameter] = []
+        for arg in args {
+            guard arg.count == 3 else {
+                continue
+            }
+
+            let fromAcct = OntologyParameter(type: .Address, value: arg[0])
+            let toAcct = OntologyParameter(type: .String, value: arg[1])
+            var type = OntologyParameterType.Integer
+            if decimals == 8 {
+                type = .Fixed8
+            } else if decimals == 9 {
+                type = .Fixed9
+            }
+            let spend = OntologyParameter(type: type, value: arg[2])
+            let array = OntologyParameter(type: .Array, value: [fromAcct, toAcct, spend])
+            params.append(array)
+        }
+
+        return ontologyInvoke(endpoint: endpoint, contractHash: contractHash, method: "transferMulti", args: params, gasPrice: gasPrice, gasLimit: gasLimit, wif: wif)
+    }
 }
